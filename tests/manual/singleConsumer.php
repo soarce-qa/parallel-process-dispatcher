b352a4001e08301816d3138352faf77dbc8dd467
Holger Segnitz <holger@segnitz.net>
2020-01-21 08:23:14 +0100

    #worksOnLinux

--- tests/manual/singleConsumer.php
+++ tests/manual/singleConsumer.php
@@ -5,16 +5,16 @@ use FastBill\ParallelProcessDispatcher\ProcessLineOutput;
 include '../../src/Process.php';
 include '../../src/ProcessLineOutput.php';
 
-$fullPath = __DIR__ . '\\outputLines.php';
+$fullPath = __DIR__ . '/outputLines.php';
 $job = new ProcessLineOutput("php -f $fullPath", "whatever");
 $job->start();
 
 echo 'x';
 
 while (!$job->isFinished()) {
-/*    while ($job->hasNextOutput()) {
-        echo $job->getNextOutput();
-    }*/
+    while ($job->hasNextOutput()) {
+        echo $job->getNextOutput() . "\n";
+    }
     echo "xxx --- done with batch, sleeping a sec.\n";
     sleep(1);
 }
